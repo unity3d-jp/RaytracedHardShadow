43b77c15a1836a2cbc90781b7a8e0cd83c82082d
i-saint <saint.skr@gmail.com>
2019-06-07 05:42:17 +0900

    packaging

---
+++ .RaytracedHardShadow/Assets/RaytracedHardShadowPackaging.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+
+public class RaytracedHardShadowPackaging
+{
+    [MenuItem("Assets/Make RaytracedHardShadow.unitypackage")]
+    public static void MakePackage()
+    {
+        string[] files = new string[]
+        {
+            "Assets/UTJ/RaytracedHardShadow",
+        };
+        AssetDatabase.ExportPackage(files, "RaytracedHardShadow.unitypackage", ExportPackageOptions.Recurse);
+    }
+
+}
+#endif // UNITY_EDITOR
