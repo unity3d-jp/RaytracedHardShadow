1aadd327500e20d1788d5c415b49a99148562609
i-saint <seiya@unity3d.com>
2019-09-06 19:42:47 +0900

    add hooks for d3d12

--- Plugin~/rths/DXR/rthsHook.h
+++ Plugin~/rths/DXR/rthsHook.h
@@ -4,9 +4,11 @@ namespace rths {
 
 using OnBufferUpdateT = std::function<void(void*)>;
 using OnBufferReleaseT = std::function<void(void*)>;
+using OnTextureReleaseT = std::function<void(void*)>;
 
 void SetOnBufferUpdate(const OnBufferUpdateT& v);
 void SetOnBufferRelease(const OnBufferReleaseT& v);
+void SetOnTextureRelease(const OnTextureReleaseT& v);
 
 // T: ID3D11DeviceContext, ID3D11Buffer
 template<class T>
