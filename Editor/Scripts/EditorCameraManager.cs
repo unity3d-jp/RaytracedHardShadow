54979b504c9e20a71ae9a5811ebc8b6ae13c6cc3
i-saint <saint.skr@gmail.com>
2019-09-08 15:31:11 +0900

    align coding convention

--- Editor/Scripts/EditorCameraManager.cs
+++ Editor/Scripts/EditorCameraManager.cs
@@ -2,63 +2,60 @@
 using UnityEngine;
 using UTJ.RaytracedHardShadow;
 
-namespace UTJ.RaytracedHardShadowEditor {
-
-
-    public static class EditorCameraManager {
-    [InitializeOnLoadMethod]
-    static void Init() {
-        Camera.onPreRender += RaytraceHardShadowPreRender;
-        Camera.onPostRender+= RaytraceHardShadowPostRender;
-    }
-
-//---------------------------------------------------------------------------------------------------------------------
-    static void RaytraceHardShadowPreRender(Camera cam)
-    {         
-        if (!IsValidCamera(cam)) return;
-
-        ShadowRaytracer shadowRaytracer = GetEditorShadowRaytracer();
-        if (null==shadowRaytracer) //No cameras has ShadowRaytracer in the game.
-            return;
-
-        shadowRaytracer.Render(cam);
-    }
-
-//---------------------------------------------------------------------------------------------------------------------
-
-    static void RaytraceHardShadowPostRender(Camera cam)
+namespace UTJ.RaytracedHardShadowEditor
+{
+    public static class EditorCameraManager
     {
-        if (!IsValidCamera(cam)) return;
+        [InitializeOnLoadMethod]
+        static void Init()
+        {
+            Camera.onPreRender += RaytraceHardShadowPreRender;
+            Camera.onPostRender += RaytraceHardShadowPostRender;
+        }
 
-        ShadowRaytracer shadowRaytracer = GetEditorShadowRaytracer();
-        if (null==shadowRaytracer) //No cameras has ShadowRaytracer in the game.
-            return;
+        static void RaytraceHardShadowPreRender(Camera cam)
+        {
+            if (!IsValidCamera(cam))
+                return;
 
-        shadowRaytracer.Finish();
+            var shadowRaytracer = GetEditorShadowRaytracer();
+            if (shadowRaytracer == null) // No cameras has ShadowRaytracer in the game.
+                return;
+
+            shadowRaytracer.Render(cam);
+        }
+
+
+        static void RaytraceHardShadowPostRender(Camera cam)
+        {
+            if (!IsValidCamera(cam))
+                return;
+
+            var shadowRaytracer = GetEditorShadowRaytracer();
+            if (shadowRaytracer == null) // No cameras has ShadowRaytracer in the game.
+                return;
+
+            shadowRaytracer.Finish();
+        }
+
+        //Only process Cameras in the Scene Window
+        static bool IsValidCamera(Camera cam)
+        {
+            return (cam.cameraType == CameraType.SceneView)
+                || (cam.cameraType == CameraType.Preview)
+                || (cam.gameObject.name == "Preview Camera");
+        }
+
+
+        static ShadowRaytracer s_usedShadowRaytracerInGame = null;
+
+        static ShadowRaytracer GetEditorShadowRaytracer()
+        {
+            if (s_usedShadowRaytracerInGame != null)
+                return s_usedShadowRaytracerInGame;
+
+            s_usedShadowRaytracerInGame = Object.FindObjectOfType<ShadowRaytracer>();
+            return s_usedShadowRaytracerInGame;
+        }
     }
-
-//---------------------------------------------------------------------------------------------------------------------
-    //Only process Cameras in the Scene Window
-    static bool IsValidCamera(Camera cam) {
-        return (CameraType.SceneView == cam.cameraType)  
-            || (CameraType.Preview== cam.cameraType)
-            || ("Preview Camera" == cam.gameObject.name);
-    }
-
-//---------------------------------------------------------------------------------------------------------------------
-
-    static ShadowRaytracer GetEditorShadowRaytracer()
-    {
-        if (null!=m_usedShadowRaytracerInGame)
-            return m_usedShadowRaytracerInGame;
-
-        m_usedShadowRaytracerInGame = Object.FindObjectOfType<ShadowRaytracer>();
-        return m_usedShadowRaytracerInGame;
-    }
-
-//---------------------------------------------------------------------------------------------------------------------
-
-    static ShadowRaytracer m_usedShadowRaytracerInGame = null;
-}
-
 } //namespace UTJ.RaytracedHardShadowEditor
