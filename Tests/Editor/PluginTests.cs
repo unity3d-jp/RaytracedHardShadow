6382dca58d3e393e2cf42311dc3f1a815f5a6cc6
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-05-18 16:54:14 +0900

    add test

---
+++ Tests/Editor/PluginTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.Windows;
+
+namespace Unity.RaytracedHardShadow.EditorTests {
+
+public class PluginTests {
+
+    [Test]
+    public void CheckPluginExist() {
+        const string PATH = "Packages/com.unity.raytracedhardshadow/Runtime/Plugins/x86_64/rths.dll";
+        Assert.True(File.Exists(PATH));
+    }
+   
+}
+}
