1a2bee4ad3597752862b1dc0a0e99e07087d933b
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-05-18 19:03:27 +0900

    fix: "Preview in Scene View" is always reset (#41)
    * Fix "Show Preview In Scene" so that it is not always set to true when changing to play mode
    
    Yamato/package settings:
    * Add CONTRIBUTING
    * Add a test to check plugin initialization
    * Yamato setting updates

---
+++ Tests/Runtime/RTHSAPITest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Unity.RaytracedHardShadow.Tests {
+internal class RTHSAPITest {
+    [Test]
+    public void RTHSInitialization() {
+        rthsRenderer renderer = rthsRenderer.Create();
+        renderer.Release();
+    }
+}
+
+} // end namespace
